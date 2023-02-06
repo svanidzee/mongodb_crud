340e18da4f32a6263430776f42c3558276e2d208
svanidzee <svanidzeee@gmail.com>
2023-02-06 16:23:31 +0300

    refactor read.js

--- create.js
+++ create.js
@@ -1,75 +1,68 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-async function main() {
-  const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
 
-  const client = new MongoClient(uri);
-
+async function run() {
   try {
-    await client.connect();
-
-    // Create a single new listing
-    await createListing(client, {
-      name: 'Lovely Loft',
-      summary: 'A charming loft in Paris',
-      bedrooms: 1,
-      bathrooms: 1
-    });
-
-    // Create 3 new listings
-    await createMultipleListinings(client, [
-      {
-        name: 'Infinite Views',
-        summary: 'Modern home with infinite views from the infinity pool',
-        property_type: 'House',
-        bedrooms: 5,
-        bathrooms: 4.5,
-        beds: 5
-      },
-      {
-        name: 'Private room in London',
-        property_type: 'Apartment',
-        bedrooms: 1,
-        bathroom: 1
-      },
-      {
-        name: 'Beautiful Beach House',
-        summary:
-          'Enjoy relaxed beach living in this house with a private beach',
-        bedrooms: 4,
-        bathrooms: 2.5,
-        beds: 7,
-        last_review: new Date()
-      }
-    ]);
-  } catch (e) {
-    console.log(e);
+    await createListing(client);
+    await createMultipleListinings(client);
   } finally {
-    await client.close;
+    await client.close();
   }
 }
+run().catch(console.dir);
 
-main().catch(console.error);
+async function createListing(client) {
+  const database = client.db('sample_airbnb');
+  const rooms = database.collection('listingsAndReviews');
 
-async function createListing(client, newListing) {
-  const result = await client
-    .db('sample_airbnb')
-    .collection('listingsAndReviews')
-    .insertOne(newListing);
-  console.log(
-    `New listing created with the following id: ${result.insertedId}`
-  );
+  // create a document to insert
+  const doc = {
+    name: 'Lovely Loft',
+    summary: 'A charming loft in Paris',
+    bedrooms: 2,
+    bathrooms: 2
+  };
+
+  const result = await rooms.insertOne(doc);
+  console.log(`A document was inserted with the _id: ${result.insertedId}`);
 }
 
-async function createMultipleListinings(client, newListings) {
-  const result = await client
-    .db('sample_airbnb')
-    .collection('listingsAndReviews')
-    .insertMany(newListings);
+async function createMultipleListinings(client) {
+  const database = client.db('sample_airbnb');
+  const rooms = database.collection('listingsAndReviews');
 
-  console.log(
-    `${result.insertedCount} new listing(s) created with the following id(s):`
-  );
-  console.log(result.insertedIds);
+  // create a document to insert
+  const docs = [
+    {
+      name: 'Infinite Views',
+      summary: 'Modern home with infinite views from the infinity pool',
+      property_type: 'House',
+      bedrooms: 5,
+      bathrooms: 4.5,
+      beds: 5
+    },
+    {
+      name: 'Private room in London',
+      property_type: 'Apartment',
+      bedrooms: 1,
+      bathroom: 1
+    },
+    {
+      name: 'Beautiful Beach House',
+      summary: 'Enjoy relaxed beach living in this house with a private beach',
+      bedrooms: 4,
+      bathrooms: 2.5,
+      beds: 7,
+      last_review: new Date()
+    }
+  ];
+
+  // this option prevents additional documents from being inserted if one fails
+  const options = { ordered: true };
+
+  const result = await rooms.insertMany(docs, options);
+  console.log(`${result.insertedCount} documents were inserted`);
 }
